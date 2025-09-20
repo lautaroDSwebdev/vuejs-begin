09515b84132c4f3c89d6de52c30cf5fc0afee838
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-09-20 17:38:21 -0300

    update practice and project

---
+++ src/components/DataComposition.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'FirstCounterComposition',
+  setup() {
+    const counter = ref(0)
+    const counterNumberInc = () => {
+      counter.value++
+      console.log(counter)
+    }
+    const counterNumberDec = () => {
+      counter.value--
+      console.log(counter)
+    }
+    const reset = () => {
+      counter.value = 0
+    }
+    return { counter, 
+        counterNumberDec,
+         counterNumberInc ,
+         reset
+        }
+  },
+})
+</script>
+
+<template>
+  <div>
+    <button @click="counterNumberDec">boton decremental</button>
+    <button @click="counterNumberInc">boton incremental</button>
+    <button @click="reset">boton reset</button>
+  </div>
+  <div>data: {{ counter }}</div>
+</template>
+
+<style></style>
---
+++ src/components/FirsComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+// import {}  from ""
+const lista = [
+  {
+    data: 'Hola',
+  },
+  {
+    data: 'Hola 1',
+  },
+  {
+    data: 'Hola 2',
+  },
+]
+</script>
+
+<template>
+  <h1>Hola</h1>
+</template>
+
+<style>
+h1 {
+  color: #000000;
+}
+</style>
